dbeb63e443a18b76043bf40cc01347279d7b2ea0
bayusamudra5502 <bayusamudra.55.02.com@gmail.com>
2022-04-14 22:16:13 +0700

    Adding similarities function with Modification of KMP Algorithm

--- src/backend/lib/similarity.go
+++ src/backend/lib/similarity.go
@@ -1,1 +1,61 @@
 package lib
+
+func jumpTableGenerator(str string) (result []int) {
+	result = append(result, 0)
+
+	for i := 1; i < len(str); i++ {
+		start := 0
+		end := i - 1
+
+		for start < end && (str[start] == str[end]) {
+			start++
+			end--
+		}
+
+		result = append(result, start)
+	}
+
+	return result
+}
+
+func Similarity(str, pattern string) float64 {
+	jumpTable := jumpTableGenerator(pattern)
+	p := 0
+	highestMatch := 0.0
+
+	for i := 0; i < len(str); {
+		if str[i] == pattern[p] {
+			p++
+
+			if p == len(pattern){
+				return 1.0
+			}
+
+			i++
+		} else {
+			matchNum := p
+			j := p + 1
+
+			if p == 0 {
+				i++
+			}
+
+			for k := i + 1; j < len(pattern) && k < len(str); k++ {
+				if str[k] == pattern[j] {
+					matchNum++
+				}
+
+				j++
+			}
+
+			newSimilarity := float64(matchNum)/float64(len(pattern))
+			if newSimilarity > highestMatch {
+				highestMatch = newSimilarity
+			}
+
+			p = jumpTable[p]
+		}
+	}
+
+	return highestMatch
+}
---
+++ src/backend/test/similarity_test.go
@@ -0,0 +1,29 @@
+package test
+
+import (
+	"testing"
+
+	"github.com/bayusamudra5502/Tubes3_13520126/src/backend/lib"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSimilarity(t *testing.T) {
+	t.Run("Exactly same", func(t *testing.T) {
+		assert.Equal(t, 1.0, lib.Similarity("ACTGAACGATGA", "ACTGA"))
+		assert.Equal(t, 1.0, lib.Similarity("ACTGA", "ACTGA"))
+		
+		assert.Equal(t, 1.0, lib.Similarity("ATTAACTGAATTA", "ACTGA"))
+		assert.Equal(t, 1.0, lib.Similarity("ACTGACTGTCTGA", "ACTGT"))
+		
+		assert.Equal(t, 1.0, lib.Similarity("ACTGAATACTGT", "ACTGT"))
+		assert.Equal(t, 1.0, lib.Similarity("AACTGATAACTGAA", "AACTGAA"))
+	})
+
+	t.Run("Patially Same", func(t *testing.T) {
+		assert.Equal(t, 0.8, lib.Similarity("ACTGAACGATGA", "ACTGG"))
+		assert.Equal(t, 0.5, lib.Similarity("ACTGA", "ATTT"))
+		assert.Equal(t, 0.0, lib.Similarity("TTTTTTT", "CCCCCC"))
+		assert.Equal(t, 0.4, lib.Similarity("ACT", "ACGCA"))
+	})
+}
