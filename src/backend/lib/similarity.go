9a331af42ca7b25df655a9e1958dbd23919de377
bayusamudra5502 <bayusamudra.55.02.com@gmail.com>
2022-04-14 22:44:49 +0700

    Fixing bugs: Some solution on the last string

--- src/backend/lib/similarity.go
+++ src/backend/lib/similarity.go
@@ -6,36 +6,41 @@ func Similarity(str, pattern string) float64 {
 	highestMatch := 0.0
 
 	for i := 0; i < len(str); {
+		var matchNum int 
+
 		if str[i] == pattern[p] {
 			p++
+			matchNum = p
 
 			if p == len(pattern){
 				return 1.0
 			}
 
 			i++
-		} else {
-			matchNum := p
+			} else {
+			matchNum = p
 			j := p + 1
 
-			if p == 0 {
-				i++
-			}
-
+			
 			for k := i + 1; j < len(pattern) && k < len(str); k++ {
 				if str[k] == pattern[j] {
 					matchNum++
 				}
-
+				
 				j++
 			}
-
-			newSimilarity := float64(matchNum)/float64(len(pattern))
-			if newSimilarity > highestMatch {
-				highestMatch = newSimilarity
+			
+			
+			if p == 0 {
+				i++
 			}
-
+			
 			p = jumpTable[p]
+		}
+
+		newSimilarity := float64(matchNum)/float64(len(pattern))
+		if newSimilarity > highestMatch {
+			highestMatch = newSimilarity
 		}
 	}
 
