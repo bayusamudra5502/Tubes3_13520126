cbce37fb6d1db73a14ad05abc23166b674896e83
ignferry <ignasius.ferry01@gmail.com>
2022-04-16 06:31:24 +0700

    Add Boyer-Moore algorithm

---
+++ src/backend/lib/boyer_moore.go
@@ -0,0 +1,31 @@
+package lib
+
+func BoyerMoore(text, pattern string) int {
+	last := BuildLast(pattern)
+	textLength := len(text)
+	patternLength := len(pattern)
+	textIndex := patternLength - 1
+
+	if textIndex > textLength-1 {
+		return -1
+	}
+
+	patternIndex := patternLength - 1
+
+	for textIndex <= textLength-1 {
+		if pattern[patternIndex] == text[textIndex] {
+			if patternIndex == 0 {
+				return textIndex
+			} else {
+				textIndex--
+				patternIndex--
+			}
+		} else {
+			lo := last[text[textIndex]]
+			textIndex = textIndex + patternLength - Min(patternIndex, 1+lo)
+			patternIndex = patternLength - 1
+		}
+	}
+
+	return -1
+}
