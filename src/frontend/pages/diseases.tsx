1a0d9b91e2998c494960cc4475e3b2545f420d3b
gilanglahat22 <gilanglahat43@gmail.com>
2022-04-29 09:21:11 +0700

    Add daftar pemeriksaan

--- src/frontend/pages/diseases.tsx
+++ src/frontend/pages/diseases.tsx
@@ -1,24 +1,23 @@
 import React, { useState } from "react";
 import IPenyakit from "../model/penyakit";
+import { Table } from './Table'
 
-export default function diseases() {
+export default function App() {
   const [data, setData] = useState<IPenyakit[]>([]);
+  const columns = [
+    { accessor: 'id', label: 'id' },
+    { accessor: 'Nama_pengguna', label: 'Nama pengguna' },
+    { accessor: 'Nama_penyakit', label: 'Nama penyakit' },
+    { accessor: 'Tanggal_pemeriksaan', label: 'Tanggal pemeriksaan' },
+    { accessor: 'Similarity', label: 'Similarity' },
+    { accessor: 'Kebenaran', label: 'Kebenaran', format: (value) => (value ? 'True' : 'False') },
+  ]
+  const rows = data;
+  return (
+    <div className="App">
+      <h1>Table Urutan Hasil Prediksi</h1>
 
-  return <div className="bg-white py-4 px-6 rounded">
-    <h1>Daftar Penyakit</h1>
-    <div>
-      <table>
-        <thead>
-          <th>
-            <td>ID</td>
-            <td>Nama Penyakit</td>
-            <td>Waktu Penambahan</td>
-          </th>
-        </thead>
-        <tbody>
-
-        </tbody>
-      </table>
+      <Table rows={rows} columns={columns} />
     </div>
-  </div>;
+  )
 }
--- src/frontend/pages/index.tsx
+++ src/frontend/pages/index.tsx
@@ -1,17 +1,18 @@
 import type { NextPage } from "next";
 import Image from "next/image";
 import Link from "next/link";
-import virus from "../public/virus.png"
+import virus from "../public/virus.png";
+import diseases from "../public/disease.jpg";
 
 const Home: NextPage = () => {
   return (
-    <div className="bg-white py-4 px-6 rounded">
+    <div className="bg-white py-10 px-12 rounded">
       <div>
         <div className="mb-5 mt-4 flex align-middle justify-center">
-          <Image src={virus} width="150px" height="150px"></Image>
+          <Image src={virus} width="200px" height="200px"></Image>
         </div>
         <h1>Disease Detector</h1>
-        <p className="font-bold">Menu Utama</p>
+        <p className="font-bold flex align-middle justify-center">Menu Utama</p>
         <div className="flex flex-col">
           <Link href="/diseases">
             <button className="p-3 w-full border-black border-2 my-2 rounded hover:text-white hover:bg-cyan-pallete transition-all ease-in-out">
